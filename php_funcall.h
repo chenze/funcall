5d14997849b13cb8c89caafdc8afbf2dfc940bca
surfchen <surfchen@569c1e30-3c42-0410-be26-fdb8343ea2e2>
2007-12-27 03:31:51 +0000

    init

---
+++ php_funcall.h
@@ -0,0 +1,94 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2007 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author:                                                              |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id: header,v 1.16.2.1.2.1 2007/01/01 19:32:09 iliaa Exp $ */
+
+#ifndef PHP_FUNCALL_H
+#define PHP_FUNCALL_H
+
+extern zend_module_entry funcall_module_entry;
+#define phpext_funcall_ptr &funcall_module_entry
+
+#ifdef PHP_WIN32
+#define PHP_FUNCALL_API __declspec(dllexport)
+#else
+#define PHP_FUNCALL_API
+#endif
+
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
+struct fc_function_list {
+    char *name,
+    zval *func,
+    fc_callback_list &callback_ref,
+    fc_function_list *next
+}
+struct fc_callback_list {
+    char *name,
+    zval *func,
+    fc_function_list *next
+}
+
+PHP_MINIT_FUNCTION(funcall);
+PHP_MSHUTDOWN_FUNCTION(funcall);
+PHP_RINIT_FUNCTION(funcall);
+PHP_RSHUTDOWN_FUNCTION(funcall);
+PHP_MINFO_FUNCTION(funcall);
+
+PHP_FUNCTION(fc_add);
+PHP_FUNCTION(fc_list);
+
+/* 
+  	Declare any global variables you may need between the BEGIN
+	and END macros here:     
+
+*/
+ZEND_BEGIN_MODULE_GLOBALS(funcall)
+    fc_function_list fc_fn_list;	
+    fc_callback_list fc_cb_list;
+ZEND_END_MODULE_GLOBALS(funcall)
+
+/* In every utility function you add that needs to use variables 
+   in php_funcall_globals, call TSRMLS_FETCH(); after declaring other 
+   variables used by that function, or better yet, pass in TSRMLS_CC
+   after the last function argument and declare your utility function
+   with TSRMLS_DC after the last declared argument.  Always refer to
+   the globals in your function as FUNCALL_G(variable).  You are 
+   encouraged to rename these macros something shorter, see
+   examples in any other php module directory.
+*/
+
+#ifdef ZTS
+#define FUNCALL_G(v) TSRMG(funcall_globals_id, zend_funcall_globals *, v)
+#else
+#define FUNCALL_G(v) (funcall_globals.v)
+#endif
+
+#endif	/* PHP_FUNCALL_H */
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
